baf175090558f1268652d08cb615c06445b51dd4
Harry Pidcock <harry.pidcock@canonical.com>
2025-11-01 07:06:10 +0100

    fix: cache mount is bound with nofail

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -35,6 +35,7 @@
     fsType = "xfs";
     options = [
       "discard"
+      "nofail"
       "defaults"
     ];
   };
@@ -47,6 +48,7 @@
     fsType = "none";
     options = [
       "bind"
+      "nofail"
     ];
   };
 
